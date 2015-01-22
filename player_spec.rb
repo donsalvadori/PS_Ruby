7579079bb242cffc5784ea6dc3f38ef994d7808b
Chris Salvi <csalvi42@gmail.com>
2015-01-22 12:45:16 -0800

    first test of the new year
    very simple test to make sure names are always capitalized in player
    class

--- player_spec.rb
+++ player_spec.rb
@@ -2,5 +2,9 @@ require_relative 'spec_helper'
 require_relative 'player'
 
 describe Player do
-	
+	it "has a capitalized name" do
+  	player = Player.new("larry", 150)
+
+  	player.name.should == "Larry"
+	end
 end
