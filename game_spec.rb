5ff0d1cbc02a12a729dc8a49cc356f409bbe4472
Chris Salvi <csalvi42@gmail.com>
2015-01-24 11:06:52 -0800

    finished game_spec and added some stubs for random

--- game_spec.rb
+++ game_spec.rb
@@ -7,5 +7,22 @@ describe Game do
 		@player = Player.new("moe",@initial_health)
 		@game.add_player(@player)
 	end
-	
+
+	it "heals the player if a high number is rolled" do
+  	allow_any_instance_of(Die).to receive(:roll).and_return(5)
+ 	 	@game.play
+  	expect(@player.health).to eq(@initial_health + 15)
+	end
+
+	it "skips the player if a medium number is rolled" do
+		allow_any_instance_of(Die).to receive(:roll).and_return(3)
+		@game.play
+		expect(@player.health).to eq(@initial_health)
+	end
+
+	it "hurts the player if a low number is rolled" do
+		allow_any_instance_of(Die).to receive(:roll).and_return(1)
+		@game.play
+		expect(@player.health).to eq(@initial_health - 10)
+	end
 end
