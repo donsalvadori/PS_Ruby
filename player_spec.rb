f2a20678972d6ed89f35067b93c11318608733a5
Chris Salvi <csalvi42@gmail.com>
2015-01-28 09:12:22 -0800

    refactored player_spec
    rspec 3 version

--- player_spec.rb
+++ player_spec.rb
@@ -72,18 +72,15 @@ describe Player do
 	end
 
 	it "computes points as the sum of all treasure points" do
-  	@player.points.should == 0
+  	expect(@player.points).to eq(0)
 
   	@player.found_treasure(Treasure.new(:hammer, 50))
-
-  	@player.points.should == 50
+  	expect(@player.points).to eq(50)
 
   	@player.found_treasure(Treasure.new(:crowbar, 400))
-  
-  	@player.points.should == 450
+  	expect(@player.points).to eq(450)
   
   	@player.found_treasure(Treasure.new(:hammer, 50))
-
-  	@player.points.should == 500
+  	expect(@player.points).to  eq(500)
 	end
 end
